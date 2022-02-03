ec8ef981a57af93c780e5567fded1ec618315560
GeffersonST <geffsevero@gmail.com>
2022-02-03 18:59:27 -0300

    Atualizando App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Listar</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import api from "./api";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Listar</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
