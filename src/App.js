4ae98514e9bf993c10f2fb8ae4ca7b9832baedbd
GeffersonST <geffsevero@gmail.com>
2022-02-03 20:09:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from "react";
-import api from "./api";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Listar</h1>
-      </div>
-    );
-  }
+function App() {
+  const [pokemonData, setPokemonData] = (useState = []);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      await axios.get("https://pokeapi.co/api/v2/pokemon");
+      console.log(response.data);
+      setPokemonData(response.data.results);
+    };
+
+    getPokemon();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        <h1>Pokedex</h1>
+
+        <li>
+          {pokemonData.map((pokemon) => {
+            return <li>{pokemon.name}</li>;
+          })}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
